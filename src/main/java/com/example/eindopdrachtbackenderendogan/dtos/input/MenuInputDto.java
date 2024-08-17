b872575378c17eb006413a0f825715dce3b3049f
erend1991 <eren.d1991@gmail.com>
2024-08-17 11:00:16 +0200

    pom.xml aangepast

--- src/main/java/com/example/eindopdrachtbackenderendogan/dtos/input/MenuInputDto.java
+++ src/main/java/com/example/eindopdrachtbackenderendogan/dtos/input/MenuInputDto.java
@@ -1,4 +1,16 @@
 package com.example.eindopdrachtbackenderendogan.dtos.input;
 
+import com.example.eindopdrachtbackenderendogan.models.Drink;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.util.List;
+
 public class MenuInputDto {
+
+    public Long id;
+    @NotEmpty()
+    public String name;
+
+    public List<Drink> drinks;
 }
