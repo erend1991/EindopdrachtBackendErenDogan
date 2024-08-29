1bf4b2c591988ec61b5c5e9bc1bf8ee4d9f24955
erend1991 <eren.d1991@gmail.com>
2024-08-29 15:22:17 +0200

    usermapper aangepast

---
+++ src/main/java/com/example/eindopdrachtbackenderendogan/dtos/output/UserOutputDto.java
@@ -0,0 +1,36 @@
+package com.example.eindopdrachtbackenderendogan.dtos.output;
+
+public class UserOutputDto {
+
+        public String username;
+
+        public String password;
+        public String[] roles;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String[] getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String[] roles) {
+        this.roles = roles;
+    }
+}
+
+
