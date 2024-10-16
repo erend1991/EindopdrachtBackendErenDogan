cf73ce40eef8927040d34066609af002c6e9a57d
erend1991 <eren.d1991@gmail.com>
2024-10-16 09:40:46 +0200

    deleted  useless imports menuinputdto

--- src/main/java/com/example/eindopdrachtbackenderendogan/dtos/input/MenuInputDto.java
+++ src/main/java/com/example/eindopdrachtbackenderendogan/dtos/input/MenuInputDto.java
@@ -1,10 +1,7 @@
 package com.example.eindopdrachtbackenderendogan.dtos.input;
 
-import com.example.eindopdrachtbackenderendogan.models.Drink;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotEmpty;
 
-import java.util.List;
 
 public class MenuInputDto {
 
